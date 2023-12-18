dc78b9a79d66cbacf6b8b990d625e99717329da7
Robi3554 <rvarga253@yahoo.com>
2023-12-18 17:44:45 +0200

    basic boss room and camera switch

---
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,64 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera firstCamera;
+    [SerializeField]
+    private CinemachineVirtualCamera secondCamera;
+
+    private CinemachineVirtualCamera currentCamera;
+
+    private float cooldownTime = 10f;
+
+    private bool canSwitch;
+
+    private void Awake()
+    {
+        currentCamera = firstCamera;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if(currentCamera == firstCamera)
+            {
+                DisableCamera(firstCamera);
+                EnableCamera(secondCamera);
+                currentCamera = secondCamera;
+                StartCoroutine(CanSwitch());
+            }
+            else if(currentCamera == secondCamera)
+            {
+                DisableCamera(secondCamera);
+                EnableCamera(firstCamera);
+                currentCamera = firstCamera;
+                StartCoroutine(CanSwitch());
+            }
+        }
+    }
+
+    private void EnableCamera(CinemachineVirtualCamera cam)
+    {
+        cam.gameObject.SetActive(true);
+    }
+
+    private void DisableCamera(CinemachineVirtualCamera cam)
+    {
+        cam.gameObject.SetActive(false);
+    }
+
+    IEnumerator CanSwitch()
+    {
+        canSwitch = false;
+
+        yield return new WaitForSeconds(cooldownTime);
+
+        canSwitch = true;
+    }
+}

41bdd949d63f497cf22d70f0aa536326f1fa3a57
Robi3554 <rvarga253@yahoo.com>
2024-04-28 21:21:00 +0300

    Gave PowerUp Spawner Full Functionality

--- Assets/Scripts/PowerUps/SpawnPowerUps.cs
+++ Assets/Scripts/PowerUps/SpawnPowerUps.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpawnPowerUps : MonoBehaviour
 {
+    public GameObject[] positions;
+
     public PowerUpList list;
 
     void Start()
     {
-        int randInt = Random.Range(0, list.powerUps.Length);
+        int randInt;
 
-        SpawnPowerUp(randInt);
+        int[] alreadyChosen = new int[positions.Length];
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            do
+            {
+              randInt = Random.Range(0, list.powerUps.Length);
+            } while (alreadyChosen.Contains(randInt));
+
+            alreadyChosen[i] = randInt;
+
+            SpawnPowerUp(randInt, i);
+        }
     }
 
-    private void SpawnPowerUp(int index)
+    private void SpawnPowerUp(int index, int n)
     {
         if (index >= 0 && index < list.powerUps.Length)
         {
             GameObject prefab = list.powerUps[index];
-            Instantiate(prefab, transform.position, Quaternion.identity);
-        }
-        else
-        {
-            Debug.LogWarning("Invalid index specified for spawning GameObject.");
+            Instantiate(prefab, positions[n].transform.position, Quaternion.identity);
         }
     }
 }
